d73224dd26ee6fa51e92e8b555654153d6e90267
Ron Korland <ronk@alicanto.com>
2014-01-24 16:28:16 +0200

    1. add test elapsed chart to test history tab 2. add more info column to suite page and test history tab 3. change test background color to current tet - test history table

--- js/controllers/TestCtrl.js
+++ js/controllers/TestCtrl.js
@@ -16,8 +16,75 @@ reportsApp.controller('TestController',
 
         TestService.getTestHistory({id: $routeParams.testId}).then(function (data) {
             $scope.testsHistory = data.tests;
+            $scope.dataArray = [];
+            $scope.catArray = [];
+            angular.forEach(data.tests, function (test) {
+                $scope.dataArray.push(test.durationSec);
+            });
+            angular.forEach(data.tests, function (test) {
+                $scope.catArray.push(test.startTime);
+            });
+            $scope.testHistoryElapsedChart();
         }, function (response) {
             $scope.testsHistory = [];
+            $scope.dataArray = [];
+            $scope.catArray = [];
             console.log(response);
         });
+
+        $scope.testHistoryElapsedChart = function () {
+            $scope.testHistoryElapsed = {
+                chart: {
+                    type: 'column',
+                    margin: [ 50, 50, 150, 80]
+                },
+                title: {
+                    text: ''
+                },
+                xAxis: {
+                    categories: $scope.catArray,
+                    labels: {
+                        rotation: -55,
+                        align: 'right',
+                        style: {
+                            fontSize: '13px',
+                            fontFamily: 'Verdana, sans-serif'
+                        }
+                    }
+                },
+                yAxis: {
+                    min: 0,
+                    title: {
+                        text: 'Elapsed (seconds)'
+                    }
+                },
+                legend: {
+                    enabled: false
+                },
+                tooltip: {
+                    pointFormat: 'Elapsed: <b>{point.y:.1f} seconds</b>',
+                },
+                series: [
+                    {
+                        name: 'Elapsed',
+                        data: $scope.dataArray,
+                        dataLabels: {
+                            enabled: true,
+                            rotation: -90,
+                            color: '#FFFFFF',
+                            align: 'right',
+                            x: 4,
+                            y: 10,
+                            style: {
+                                fontSize: '13px',
+                                fontFamily: 'Verdana, sans-serif',
+                                textShadow: '0 0 3px black'
+                            }
+                        }
+                    }
+                ]
+            };
+        };
+
+
     });
